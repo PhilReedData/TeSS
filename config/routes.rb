bd16268cb3bd4c821a71d920d80652d738c634e4
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-05-02 10:49:57 +0100

    Community portal proof-of-concept

--- config/routes.rb
+++ config/routes.rb
@@ -116,6 +116,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :communities, only: [:show]
+
   get 'elearning_materials' => 'materials#index', defaults: { 'resource_type' => 'e-learning' }
 
   get 'invitees' => 'users#invitees'
